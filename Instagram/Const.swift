ce9be774668c333bc4983e8fef6546ef820494da
MasaharuHoshino <masaharu_hoshino@icloud.com>
2022-11-19 11:50:45 +0900

    Lesson 8終了

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by Masaharu Hoshino (Work) on 2022/11/17.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
